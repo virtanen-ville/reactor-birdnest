0c592a3b8f95085e0b5c696cd63a5b301fbc838b
Ville Virtanen <ville.o.virtanen@icloud.com>
2022-12-09 10:34:30 +0200

    Layout changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import xml2js from "xml2js";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,7 +8,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { io } from "socket.io-client";
-const parser = new xml2js.Parser();
+import { Container, Typography } from "@mui/material";
 export interface Drone {
 	serialNumber: string;
 	timestamp: string;
@@ -69,60 +68,66 @@ function App() {
 	}, [socket]);
 
 	return (
-		<div className="App">
-			<h1>Birdnest</h1>
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label="drone-info-table">
-					<TableHead>
-						<TableRow>
-							<TableCell>Latest observation</TableCell>
-							<TableCell align="right">Latest in NDZ</TableCell>
+		<Container maxWidth="lg">
+			<Typography variant="h2">Birdnest</Typography>
 
-							<TableCell align="right">Drone S/N</TableCell>
-							<TableCell align="right">
-								Closest Distance
-							</TableCell>
+			<Paper sx={{ width: "100%", overflow: "hidden" }}>
+				<TableContainer sx={{ maxHeight: "80vh" }}>
+					<Table
+						stickyHeader
+						sx={{ minWidth: 650 }}
+						aria-label="drone-info-table"
+					>
+						<TableHead>
+							<TableRow>
+								<TableCell>Latest observation</TableCell>
+								<TableCell align="right">
+									Latest in NDZ
+								</TableCell>
 
-							<TableCell align="right">Name</TableCell>
-							<TableCell align="right">Phone</TableCell>
-							<TableCell align="right">Email</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{drones &&
-							drones.map((drone, idx) => (
-								<TableRow key={idx}>
-									<TableCell component="th" scope="row">
-										{new Date(
-											drone.timestamp
-										).toLocaleString()}
-									</TableCell>
-									<TableCell align="right">
-										{new Date(
-											drone.NDZtimestamp || ""
-										).toLocaleString()}
-									</TableCell>
-									<TableCell align="right">
-										{drone.serialNumber}
-									</TableCell>
-									<TableCell align="right">
-										{drone.distance.toFixed(2)} meters
-									</TableCell>
-									<TableCell align="right">
-										{`${drone.owner.firstName} ${drone.owner.lastName}`}
-									</TableCell>
-									<TableCell align="right">
-										{drone.owner.phoneNumber}
-									</TableCell>
-									<TableCell align="right">
-										{drone.owner.email}
-									</TableCell>
-								</TableRow>
-							))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</div>
+								<TableCell align="right">
+									Closest Distance
+								</TableCell>
+
+								<TableCell align="right">Name</TableCell>
+								<TableCell align="right">Phone</TableCell>
+								<TableCell align="right">Email</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{drones &&
+								drones.map((drone, idx) => (
+									<TableRow key={idx}>
+										<TableCell component="th" scope="row">
+											{new Date(
+												drone.timestamp
+											).toLocaleString()}
+										</TableCell>
+										<TableCell align="right">
+											{new Date(
+												drone.NDZtimestamp || ""
+											).toLocaleString()}
+										</TableCell>
+
+										<TableCell align="right">
+											{drone.distance.toFixed(2)} meters
+										</TableCell>
+										<TableCell align="right">
+											{`${drone.owner.firstName} ${drone.owner.lastName}`}
+										</TableCell>
+										<TableCell align="right">
+											{drone.owner.phoneNumber}
+										</TableCell>
+										<TableCell align="right">
+											{drone.owner.email}
+										</TableCell>
+									</TableRow>
+								))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Paper>
+		</Container>
 	);
 }
 
